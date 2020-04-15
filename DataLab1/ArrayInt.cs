512c33ac116293a4d6924bf5605f8ab1e1fe8428
SchultzW <36714268+SchultzW@users.noreply.github.com>
2020-04-15 09:06:27 -0700

    init

---
+++ DataLab1/ArrayInt.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataLab1
+{
+    public class ArrayInt
+    {
+        public int Size { get; set; }
+        private int[] myArray;
+        public int[] MyArray { get { return myArray; } }
+        
+        public ArrayInt() { }
+        public ArrayInt(int size)
+        {
+            myArray = new int[size];
+        }
+        //public void ArrayInt(int size)
+        //{
+        //    if (size == 0)
+        //    {
+        //        int[] myArray = new int[10];
+        //    }
+        //    else
+        //    {
+        //        int[] myArray = new int[size];
+        //    }
+            
+            
+                
+        //}
+        //returns the element at index
+        public int GetAt(int index)
+        {
+            try
+            {
+                return myArray[index];
+            }
+            catch
+            {
+                throw new System.ArgumentException("GetAt: Out of Range");
+            }
+            
+        }
+        //sets the value at a vertain index
+        public void SetAt(int index, int value)
+        {
+            try
+            {
+                myArray.SetValue(value, index);
+            }
+            catch
+            {
+                throw new System.ArgumentException("SetAt: Out of Range");
+            }
+        }
+        //return the size
+        public int GetSize()
+        {
+            return myArray.Length - 1;
+        }
+        //resize the array
+        public void SetSize(int size)
+        {
+            Array.Resize(ref myArray, size);
+        }
+        //add val at next empty or 2X array and add
+        public void Append(int value)
+        {
+            int empty = FindAt();
+            if(empty==-1)
+            {
+                SetSize(GetSize()*2);
+                empty = FindAt();
+                myArray[empty] = value;
+            }
+            else
+            {
+                myArray[empty] = value;
+            }
+        }
+        //0 indicates array element is empty
+        public int FindAt()
+        {
+            int j = 0;
+            foreach(int i in myArray)
+            {
+               
+                if (i == 0)
+                    return j;
+                j++;
+
+            }
+            return -1;
+        }
+        //inserts value at location and shift all values. if full 2X
+        public void InsertAt(int index, int value)
+        {
+            try
+            {
+                int empty = FindAt();
+                if (empty == -1)
+                {
+                    int[] arrayEnd = new int[(myArray.Length) - index];
+                    //myArray.CopyTo(arrayEnd, index);
+                    Array.Copy(myArray,index,arrayEnd,0,arrayEnd.Length);
+                    myArray[index] = value;
+                    SetSize((myArray.Length) * 2);
+                    arrayEnd.CopyTo(myArray, index + 1);
+                }
+                else
+                {
+
+                    int[] arrayEnd = new int[(myArray.Length-1)-index];
+                    Array.Copy(myArray, index, arrayEnd, 0, arrayEnd.Length);
+                    myArray[index] = value;
+                    arrayEnd.CopyTo(myArray, index + 1);
+
+
+                }
+            }
+            catch
+            {
+                throw new System.ArgumentException("InsertAt:Out of Range");
+            }
+
+        }
+        //remove at index and shift all elements down
+        public int RemoveAt(int index)
+        {
+            try
+            {
+                int val = GetAt(index);
+                //copy everything starting from index
+                int[] arrayEnd = new int[(myArray.Length - 1) - index];
+
+                //myArray.CopyTo(arrayEnd, index+1);
+                //arrayEnd.CopyTo(myArray, index);
+                Array.Copy(myArray, index+1, arrayEnd, 0, arrayEnd.Length);               
+                Array.Copy(arrayEnd, 0, myArray, index,arrayEnd.Length);
+
+                myArray[myArray.Length - 1] = 0;
+
+                return val;
+            }
+            catch
+            {
+                throw new System.ArgumentException("RemoveAt:Out of Range");
+            }
+
+        }
+    }
+}
---
+++ DataLab1/Program.cs
@@ -0,0 +1,329 @@
+﻿using System;
+
+
+namespace DataLab1
+{
+    class Program
+    {
+        public static ArrayInt myArray;
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            int size = SetSize();
+            myArray = new ArrayInt(size);
+            Console.WriteLine("How many numbers do you want to put into your array? Whole numbers only...");
+            int input = int.Parse(Console.ReadLine());
+            FillArray(input);
+            DisplayArray(myArray);
+            TestGetAt();
+            TestSetAt();
+            TestGetSize();
+            TestSetSize();
+            TestAppend();
+            TestInsertAt();
+            TestRemoveAt();
+            Console.WriteLine("Finished!");
+            
+        }
+        public static int SetSize()
+        {
+            
+            Console.WriteLine("How large an Array would you like to make?");
+            try
+            {
+                
+                string input= Console.ReadLine();
+                int size=CheckSize(input);
+                return size;
+            }
+            catch
+            {
+                return -1;
+            }
+        }
+        public static int CheckSize(string input)
+        {
+            
+            try
+            {
+                int size = int.Parse(input);
+                return size;
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong...");
+                SetSize();
+            }
+            return -1;
+           
+            
+        }
+        public static void FillArray(int size)
+        {
+            string test = "FillArray";
+            Random random = new Random();
+            try
+            {
+                
+                for (int i = 0; i <= size-1; i++)
+                {
+                    int num = random.Next(1, 99);
+                    myArray.SetAt(i, num);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("You broke me. All you had to do was type in a number what is so hard about that?");
+                //Start process, friendly name is something like MyApp.exe (from current bin directory)
+                TestAgain("FillArray");
+            }
+        }
+        public static void DisplayArray(ArrayInt a)
+        {
+            string message = "";
+            int j = 1;
+            Console.WriteLine("Your array has:");
+            for (int i = 0; i <= a.GetSize(); i++, j++)
+            {
+                if (j % 5 == 0)
+                {
+                    j = 0;
+                    message += " " + a.GetAt(i).ToString() + System.Environment.NewLine;
+
+                }
+                else
+                {
+
+                    message += " " + a.GetAt(i).ToString() + " ";
+                }
+            }
+            Console.Write(message + System.Environment.NewLine);
+        }
+        public static void TestGetAt()
+        {
+            string test = "GetAt";
+            Console.WriteLine("We will test GetAt. Which Index Would you like to see?");
+            try
+            {
+                int input = int.Parse(Console.ReadLine());
+                int output = myArray.GetAt(input);
+                Console.WriteLine("You selected index " + input + " which returned " + output);
+                TestAgain(test);
+                
+            }
+            catch
+            {
+                ErrorAsk(test);
+
+            }
+            
+        }
+        public static void TestAgain(string test)
+        {
+            Console.WriteLine("Would you like to try again? Yes or no");
+            string input = Console.ReadLine();
+            try
+            {
+                input = input.ToLower();
+                input.Trim();
+                if (input == "yes")
+                {
+                    switch(test)
+                    {
+                        case "GetAt":
+                            TestGetAt();
+                            break;
+                        case "SetAt":
+                            TestSetAt();
+                            break;
+                        case "GetSize":
+                            TestGetSize();
+                            break;
+                        case "Append":
+                            TestAppend();
+                            break;
+                        case "SetSize":
+                            TestSetSize();
+                            break;
+                        case "InsertAt":
+                            TestInsertAt();
+                            break;
+                        case "RemoveAt":
+                            TestRemoveAt();
+                            break;
+                        case "FillArray":
+                            SetSize();
+                            break;
+                        default:
+                            Console.WriteLine("Case Statement Failure");
+                            break;                          
+                    }
+                }
+                else if (input == "no")
+                {
+                    Console.WriteLine("Moving to next test");
+                    Console.WriteLine("////////////////////////////////////////////////////////////////");
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Something went wrong, its not you its me....");
+                //Start process, friendly name is something like MyApp.exe (from current bin directory)
+                System.Diagnostics.Process.Start(System.AppDomain.CurrentDomain.FriendlyName);
+
+                //Close the current process
+                Environment.Exit(0);
+            }
+        }
+        public static void ErrorAsk(string test)
+        {
+            Console.WriteLine("Somthing is wrong with your input try again with a whole number.");
+            switch (test)
+            {
+                case "GetAt":
+                    TestGetAt();
+                    break;
+                case "SetAt":
+                    TestSetAt();
+                    break;
+                case "GetSize":
+                    TestGetSize();
+                    break;
+                case "Append":
+                    TestAppend();
+                    break;
+                case "SetSize":
+                    TestSetSize();
+                    break;
+                case "InsertAt":
+                    TestInsertAt();
+                    break;
+                case "RemoveAt":
+                    TestRemoveAt();
+                    break;
+                case "FillArray":
+                    SetSize();
+                    break;
+                default:
+                    Console.WriteLine("Case Statement Failure");
+                    break;
+            }
+
+        }
+        public static void TestSetAt()
+        {
+            string test = "SetAt";
+            Console.WriteLine("We will test SetAt. Which Index Would you like to change?");
+            DisplayArray(myArray);
+            try
+            {
+                int index=int.Parse(Console.ReadLine());
+                Console.WriteLine("What value?(between 1-99)");
+                int val = int.Parse(Console.ReadLine());
+                myArray.SetAt(index, val);
+                Console.WriteLine("Here is your new array:");
+                DisplayArray(myArray);
+                TestAgain(test);
+            }
+            catch
+            {
+                
+                ErrorAsk(test);
+            
+            }
+        }
+        public static void TestGetSize()
+        {
+            string test = "GetSize";
+            Console.WriteLine("Now testing GetSize");
+            try
+            {
+                int size = myArray.GetSize();
+                Console.WriteLine("Your array's size is:" + size);
+                DisplayArray(myArray);
+                TestAgain(test);
+            }
+            catch
+            {
+                ErrorAsk(test);
+            }
+        }
+        public static void TestAppend()
+        {
+            string test = "Append";
+            Console.WriteLine("Now we will append a value to your array. Enter a number.");
+            try
+            {
+               int val= int.Parse(Console.ReadLine());
+
+                myArray.Append(val);
+                DisplayArray(myArray);
+                TestAgain(test);
+            }
+            catch
+            {
+                ErrorAsk(test);
+            }
+        }
+        public static void TestInsertAt()
+        {
+            string test = "InsertAt";
+            Console.WriteLine("We will now test InsertAt at a index location");
+            try
+            {
+                Console.WriteLine("What number would you like to insert?");
+                int val = int.Parse(Console.ReadLine());
+                Console.WriteLine("What index would you like to insert?");
+                int index= int.Parse(Console.ReadLine());
+                myArray.InsertAt(index, val);
+                DisplayArray(myArray);
+                TestAgain(test);
+            }
+            catch
+            {
+                ErrorAsk(test);
+            }
+        }
+        public static void TestSetSize()
+        {
+            string test = "SetSize";
+
+            Console.WriteLine("We will change the size of the array. Currently your size is"+myArray.GetSize());
+            try
+            {
+                Console.WriteLine("What is the new size?");
+                int val = int.Parse(Console.ReadLine());
+                myArray.SetSize(val);
+                int size=myArray.GetSize();
+                Console.WriteLine("Size is " + size);
+                DisplayArray(myArray);
+                TestAgain(test);
+            }
+            catch
+            {
+                ErrorAsk(test);
+            }
+
+
+        }
+        public static void TestRemoveAt()
+        {
+            string test = "RemoveAt";
+
+            Console.WriteLine("We will now remove a value at an index");
+            try
+            {
+                Console.WriteLine("Where would you like to remove from?");
+                int val = int.Parse(Console.ReadLine());
+                int ele=myArray.RemoveAt(val);
+                Console.WriteLine(ele + " was removed");
+                DisplayArray(myArray);
+                TestAgain(test);
+            }
+            catch
+            {
+                ErrorAsk(test);
+            }
+        }
+    }
+}
